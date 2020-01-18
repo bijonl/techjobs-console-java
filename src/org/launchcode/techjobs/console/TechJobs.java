0db6519df253c14459aa54f22c40261fb3c09878
bijonl <bijon2591@gmail.com>
2020-01-18 13:17:51 -0600

    Completed Print Jobs part

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -110,7 +111,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if(someJobs.size() == 0) {
+            System.out.println("No Jobs Here");
+        } else {
+            for(HashMap<String, String> jobListing : someJobs) {
+                System.out.println("*****");
+                for(Map.Entry<String,String> jobEntry : jobListing.entrySet()) {
+                    System.out.println(jobEntry.getKey() + ": " + jobEntry.getValue());
+                }
+                System.out.println("***** \n");
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        }
+
     }
 }
